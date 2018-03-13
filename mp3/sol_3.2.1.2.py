1bda0f6d826fa92b73b8433a382f8ccd43b9c9f3
Sujay <sujay.patwardhan@gmail.com>
2018-03-12 23:02:48 -0500

    added profanity

--- mp3/sol_3.2.1.2.py
+++ mp3/sol_3.2.1.2.py
@@ -3,28 +3,27 @@ from pymd5 import md5, padding
 from urllib import quote
 
 def attack(query, cmd):
-    idx = query.find('&')
-    token = query[:idx].split('=')[1]
+    end_token = query.find('&')
+    token = query[:end_token].split('=')[1]
+    params = query[end_token+1:]
 
-    cmds = query[idx+1:]
+    pad_bytes = padding((8 + len(params)) * 8)
 
-    pad = padding((8 + len(cmds)) * 8)
-
-    before = md5(state=token.decode('hex'), count=512)
-    before.update(cmd)
-    hashed = before.hexdigest()
+    kms = md5(state=token.decode('hex'), count=512) # set start state to end of user string
+    kms.update(cmd)
+    hashed = kms.hexdigest()
     print(hashed)
 
-    return "token=" + hashed + "&" + cmds + quote(pad) + cmd
+    return "token=" + hashed + "&" + params + quote(pad_bytes) + cmd
 
 
-with open(sys.argv[1], 'r') as f1:
-    q = f1.read().strip()
+with open(sys.argv[1], 'r') as fuck:
+    query_string = fuck.read().strip()
 
-with open(sys.argv[2], 'r') as f2:
-    cmd = f2.read().strip()
-    res = attack(q, cmd)
+with open(sys.argv[2], 'r') as shit:
+    kys = shit.read().strip()
+    result = attack(query_string, kys)
 
-with io.FileIO(sys.argv[3], "w") as out:
-    out.write(res)
+with io.FileIO(sys.argv[3], 'w') as output:
+    output.write(result)
 
